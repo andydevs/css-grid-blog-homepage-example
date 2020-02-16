6402363950377a62839214ebb00e9c4dcb572e80
andydevs <akanshul97@gmail.com>
2020-02-16 17:26:51 -0500

    Got rid of the other stuff in js and scss

--- _public/main.js
+++ _public/main.js
@@ -1,11 +1,2 @@
 "use strict";
-
-function onBodyLoad() {
-  var element = document.getElementsByTagName('h1')[0];
-  console.log(element);
-  element.addEventListener('click', function (event) {
-    element.classList.add('bigboi');
-    element.innerHTML = 'OYEEEEE';
-  });
-}
 //# sourceMappingURL=main.js.map
--- js/main.js
+++
@@ -1,8 +0,0 @@
-function onBodyLoad() {
-    let element = document.getElementsByTagName('h1')[0]
-    console.log(element)
-    element.addEventListener('click', function(event) {
-        element.classList.add('bigboi');
-        element.innerHTML = 'OYEEEEE';
-    })
-}
